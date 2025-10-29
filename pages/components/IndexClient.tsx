a34f9d1b0346178df8356ba24e280935bff6652a
masterpiece-alliance <leeyoon@ma-cc.co.kr>
2025-10-30 02:56:04 +0900

    Update IndexClient adjustments

--- pages/components/IndexClient.tsx
+++ pages/components/IndexClient.tsx
@@ -25,14 +25,14 @@ const IndexClient = () => {
 
   // 캔버스(저장용) 폰트 옵션 + 위치 미세 조정(디자인 px 기준)
   const CANVAS_FONT_OPTIONS = useMemo(() => ({
-    to: { weight: 400, manualSize: 60, xOffset: -110, yOffset: -5 },
-    from: { weight: 400, manualSize: 60, xOffset: 145, yOffset: -5 },
+    to: { weight: 400, manualSize: 60, xOffset: -120, yOffset: -5 },
+    from: { weight: 400, manualSize: 60, xOffset: 135, yOffset: -5 },
   }), []);
 
   // 메인 화면 오버레이(입력창)에만 적용할 프리뷰 폰트 옵션 (작게 표시)
   const OVERLAY_FONT_OPTIONS = useMemo(() => ({
-    to: { weight: 400, manualSize: 60, xOffset: -110, yOffset: -5 },
-    from: { weight: 400, manualSize: 60, xOffset: 145, yOffset: -5 },
+    to: { weight: 400, manualSize: 60, xOffset: -120, yOffset: -5 },
+    from: { weight: 400, manualSize: 60, xOffset: 135, yOffset: -5 },
   }), []);
 
   const updateLandingFontScale = useCallback(() => {
